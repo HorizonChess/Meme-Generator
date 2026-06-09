892cd9647c1f8101ae1edca11ea992e645ac2c2f
Roseman <xeleton@gmail.com>
2026-06-09 17:46:33 +0300

    init

---
+++ js/services/meme-service.js
@@ -0,0 +1,14 @@
+let gImgs = [{ id: 1, url: 'resources/meme-imgs/1.jpg', keywords: ['funny', 'cat'] }]
+
+let gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
